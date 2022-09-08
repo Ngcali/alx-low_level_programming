994aab1035ec795f7642bc538ae2a5dd51a162f3
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-08 10:13:45 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main()
+/**
+ *The main function should probably be void.
+ * 
+ *Description: This function prints "Programming is like building a multilingual puzzle"
+ * 
+ *All C programs always return 0
+ * /
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return 0;
 }
