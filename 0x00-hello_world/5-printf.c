958e1b65c9c9cc3c1d850473055143dd7d11c1bf
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-08 11:17:16 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints "with proper grammar, but the outcome is a piece of art,"
+ *
+ * Return: All C programs always return 0
+ */
+int main(void)
+{
+	printif("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
