4a9fd79d231b3f3d7cb7e6427b9c1055647498d8
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-08 10:33:00 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- *The main function should probably be void.
- * 
- *Description: This function prints "Programming is like building a multilingual puzzle"
- * 
- *All C programs always return 0
+ * The main function should probably be void.
+ *
+ * Description: Function prints "Programming is like building a multilingual puzzle"
+ *
+ * All C programs always return 0
  */
 int main(void)
 {
