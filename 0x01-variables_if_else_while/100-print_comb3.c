870b5a2f69ca5f264e7cd6e3f9c9fc38570d33cc
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-11 13:28:03 -0700

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point.
+ *
+ * Descreption: print all possible different
+ * combinations of two digits.
+ *
+ * Return: C program always returns 0.
+ */
+int main(void)
+{
+	int my_number_one, my_number_two = 48;
+
+	while (my_number_one < 58)
+	{
+		my_number_two = my_number_one + 1;
+
+		while (my_number_two)
+		{
+			putchar(my_number_one);
+			putchar(my_number_two);
+
+			if (my_number_one < 56 || my_number_two < 57)
+			{
+				putchar(44);
+				putchar(32);
+			}
+			my_number_two++;
+		}
+		my_number_one++;
+	}
+}
