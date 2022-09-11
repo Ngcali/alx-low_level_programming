ef2796dd2a1bfa030a0dcb1d3b422ec2990a4836
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-11 02:05:44 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point.
+ *
+ * Description: prints the alphabet in lowercase.
+ *
+ * Return: C program always return 0.
+ */
+int main(void)
+{
+	int my_a = 97;
+	while(my_a < 122)
+	{
+		putchar(my_a);
+		my_a++;
+	}
+	putcar(10);
+	return (0);
+}
