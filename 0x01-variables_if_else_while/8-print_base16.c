83cc5a12cb018606dc86c6b59dafd9101d9b77ab
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-11 07:28:24 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - the entry point.
+ *
+ * Description: print all the numbers of base 16
+ * in lowercase, followed by a new line.
+ *
+ * Return: C program always returns 0.
+ */
+int main(void)
+{
+	my_number = 48;
+	my_a = 97;
+	
+	while (my_number < 58)
+	{
+		putchar(my_number);
+		my_number++;
+	}
+
+	while (my_a < 103)
+	{
+		putchar(my_a);
+		my_a++;
+	}
+	putchar(10);
+	return (0);
+}
