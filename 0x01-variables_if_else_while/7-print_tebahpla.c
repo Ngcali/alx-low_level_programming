2408f2ccb721451a4f2a512036007b2922c5ef04
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-11 05:06:50 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - entry point.
+ *
+ * Description: print the lowercase alphabet in
+ * reverse, followed by a new line.
+ *
+ * Return: C program always returns 0.
+ */
+int main(void)
+{
+	int my_z = 122;
+
+	while (my_z > 96)
+	{
+		putchar(my_z);
+		my_--;
+	}
+	putchar(my_z);
+	return (0);
+}
