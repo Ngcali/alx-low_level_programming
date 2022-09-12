383f2167cda0c7097f83a734b9548d73ca672a18
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-12 04:53:26 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,43 +10,43 @@
  */
 int main(void)
 {
-	int my_number_one, my_number_two, my_number_three, my_number_four;
+	int my_num_one, my_num_two, my_num_three, my_num_four;
 	
-	my_number_one = 48;
+	my_num_one = 48;
 
-	while (my_number_one < 58)
+	while (my_num_one < 58)
 	{
-		my_number_two = 48;
+		my_num_two = 48;
 
-		while (my_number_two < 58)
+		while (my_num_two < 58)
 		{
-			my_number_four = my_number_two + 1;
+			my_num_four = my_num_two + 1;
 
-			my_number_three = my_number_one;
+			my_num_three = my_num_one;
 
-			while (my_number_three < 58)
+			while (my_num_three < 58)
 			{
-				while (my_number_four < 58)
+				while (my_num_four < 58)
 				{
-					putchar(my_number_one);
-					putchar(my_number_two);
+					putchar(my_num_one);
+					putchar(my_num_two);
 					putchar(32);
-					putchar(my_number_three);
-					putchar(my_number_four);
+					putchar(my_num_three);
+					putchar(my_num_four);
 
-					if (my_number_one < 57 || my_number_two < 56 || my_number_three < 57 || my_number_one < 57)
+					if (my_num_one < 57 || my_num_two < 56 || my_num_three < 57 || my_num_four < 57)
 					{
 						putchar(44);
 						putchar(32);
 					}
-					my_number_four++;
+					my_num_four++;
 				}
-				my_number_four = 48;
-				my_number_three++;
+				my_num_four = 48;
+				my_num_three++;
 			}
-			my_number_two++;
+			my_num_two++;
 		}
-		my_number_one++;
+		my_num_one++;
 	}
 	putchar(10);
 	return (0);
