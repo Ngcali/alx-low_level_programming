20cd93b81a2114c390f36793d4b9d0b08b5094ed
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-13 05:11:09 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - the entry point.
+ *
+ * Description: print _putchar, followed by a new line.
+ *
+ * Return: C program always returns 0.
+ */
+int main(void)
+{
+	char my_string[10] = "_putchar";
+	int my_number = 0;
+
+	while (my_number < 9)
+	{
+		_putchar(my_string[my_number]);
+		my_number++;
+	}
+	_putchar('\n');
+	return (0);
+}
