edd1f320645a0e419466138296d8cd3b058372ed
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-13 16:15:56 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - the entry point.
+ *
+ * Description: prints the alphabet, in
+ * lowercase, followed by a new line.
+ *
+ * Return: C program always returns 0.
+ */
+int main(void)
+{
+	char my_num = 'a';
+
+	while (my_num < 'z')
+	{
+		_putchar(my_num);
+		my_num++;
+	}
+	_putchar('\n');
+	return (0);
+}
