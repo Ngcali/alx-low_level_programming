a46100d70932b229f033ceb35854a4e5f8cf1e2e
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-14 03:55:36 -0700

    Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -33,8 +33,8 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar(my_units + '0');
 				_putchar(my_tens + '0');
+				_putchar(my_units + '0');
 			}
 		}
 		_putchar('\n');
