e7b1d5f4e2b154c55fcaa24aa3169079ac8581e2
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-14 03:48:49 -0700

    Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table,
+ * starting with 0.
+ *
+ * Return: void.
+ */
+void times_table(void)
+{
+	int my_column, my_row, my_units, my_tens, my_product;
+
+	for (my_row = 0; my_row <= 9; my_row++)
+	{
+		for (my_column = 0; my_column <= 9; my_column++)
+		{
+			my_product = my_column * my_row
+			my_units = my_product % 10;
+			my_tens = my_product / 10;
+
+			if (my_column == 0)
+			{
+				_putchar(0);
+			}
+			else if (my_product < 10)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(my_units + '0');
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				-putchar(my_units + '0');
+				_putchar(my_tens + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
