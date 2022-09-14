86c29fbf260c58193e828fd36c5f4306fc5151bd
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-14 00:14:56 -0700

    There is no such thing as absolute value in this world. You can only estimate what a thing is worth to you

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer.
+ *
+ * @my_int: is a variable of type integer.
+ *
+ * Return: absolute value of an integer.
+ */
+int _abs(int my_int);
+{
+	if (my_int > 0)
+	{
+		return (my_int);
+	}
+	if else (my_int < 0)
+	{
+		return (-my_int);
+	}
+	else
+	{
+		return (my_int);
+	}
+}
