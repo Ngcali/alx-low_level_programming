90c304679c0581c566f799d81b233215615cab85
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-14 05:17:26 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - adds two integers.
+ *
+ * @int_one: is a variable of type integer.
+ * @int_two: is a variable of type integer.
+ *
+ * Return: returns the result.
+ */
+int add(int int_one, int int_two)
+{
+	int int_result = int_one + int_two;
+
+	return (int_result);
+}
