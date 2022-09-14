672a5ebecd980559bc5882b2e4612316f513d1fc
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-14 07:57:41 -0700

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers
+ * from n to 98, followed by a new line.
+ *
+ * @n: is a variable of type integer.
+ *
+ * Return: void.
+ */
+void print_to_98(int n)
+{
+	if (n < 98)
+	{
+		while (n < 98)
+		{
+			_putchar(n);
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n > 98)
+		{
+			_putchar(n);
+			n--;
+		}
+	}
+	_putchar('98/n');
+}
