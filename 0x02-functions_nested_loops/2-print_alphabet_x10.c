5b271787a998012b8f35fcab03de346bc3646391
Ngcali Alexa Magopeni <magopeningcali@gmail.com>
2022-09-13 17:15:08 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,5 +10,18 @@
  */
 void print_alphabet_x10(void)
 {
-	
+	char my_letter;
+	int my_iteration = 1;
+
+	while (my_iteration <= 10)
+	{
+		my_letter = 'a';
+
+		while (my_letter <= 'z')
+		{
+			-putchar(my_letter);
+			my_letter++;
+		}
+		_putchar('\n');
+	}
 }
