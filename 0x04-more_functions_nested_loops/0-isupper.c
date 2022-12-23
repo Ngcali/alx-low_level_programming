fb6883684f646ef55a0265331c0c645b4a09d1e9
Ngcali <magopeningcali@gmail.com>
2022-12-23 11:39:13 -0800

    0. isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/*
+ * _isupper - checks for uppercase character.
+ * @c: integer.
+ * Return: 1 if c is uppercase.
+ * Return: 0 otherwise.
+ */
+
+int _isupper(int c)
+{
+    if (x <= 90 && x >=65)
+    {
+        return (1);
+    }
+    else
+    {
+        return (0);
+    }
+}
