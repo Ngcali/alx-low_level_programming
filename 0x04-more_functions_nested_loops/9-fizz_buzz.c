afc17606e3e697874e03065306fdd60ebcf9967e
Ngcali <magopeningcali@gmail.com>
2022-12-24 08:20:43 -0800

    9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * fizz_buzz - prints Fizz for
+ * multiples of 3 and Buzz for
+ * multples of 5. For multiples
+ * of both 3 and 5, prints FizzBuzz.
+ */
+
+int fizz_buzz(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", i);
+		}
+
+		if (x != 100)
+		{
+			_putchar(32);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
