c4595295c1eb8d8a87bb26a0cb1f0298cd0efcad
Ngcali <magopeningcali@gmail.com>
2022-12-24 01:05:34 -0800

    5. Numbers constitute the only universal language

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times
+ * the numbers, from 0 to 14.
+ *
+ * Return: Numbers from 0 to 14,
+ * 10 times.
+ */
+
+void more_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (a = 0; a <= 14; a++)
+		{
+			if (a > 9)
+			{
+				_putchar((a / 10) + '0');
+			}
+
+			_putchar((a % 10) + '0');
+		}
+
+		_putchar(i + '0');
+	}
+
+
+}
