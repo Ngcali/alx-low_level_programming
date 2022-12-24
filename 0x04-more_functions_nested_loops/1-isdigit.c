8f4a32548826eab31a51fd65493317e22eb69969
Ngcali <magopeningcali@gmail.com>
2022-12-23 22:06:02 -0800

    1. isdigit

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,22 +1,22 @@
 #include "main.h"
 
-/**
+/*
  * _isdigit - a function that checks
  * for a digit (0 through 9).
  * @c: is the intenteger that is
  * checked.
- * Return: 1 if c is a digit.
- * Return: 0 otherwise.
+ * Return: 1 if c is a digit,
+ * 0 otherwise.
  */
 
 int _isdigit(int c)
 {
-    if (c <= 57 && c >=48)
-    {
-        return (1);
-    }
-    else
-    {
-        return (0);
-    }
+	if (c <= 57 && c >=48)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
