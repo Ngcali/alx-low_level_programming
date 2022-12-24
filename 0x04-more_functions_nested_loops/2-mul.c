dd0f67ebf8b47356c7c37ff33622996f6ecc581e
Ngcali <magopeningcali@gmail.com>
2022-12-23 22:40:07 -0800

    2. Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers.
+ *
+ * @a: first number.
+ * @b: second number to multiply
+ * with.
+ *
+ * Return: a*b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
