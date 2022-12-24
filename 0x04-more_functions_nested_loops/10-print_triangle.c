8d5e2ef010022cebd609af28ca8801758b911d14
Ngcali <magopeningcali@gmail.com>
2022-12-24 09:42:37 -0800

    10. Triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,24 +8,22 @@
 
 void print_triangle(int size)
 {
+	int a, b, c;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int a;
 
 		for (a = 0; a <= size; a++)
 		{
-			int b;
 
 			for (b = size - a; b > 1; b++)
 			{
 				_putchar(32);
 			}
-
-			int c;
 
 			for (c = 0; c <= a; c++)
 			{
