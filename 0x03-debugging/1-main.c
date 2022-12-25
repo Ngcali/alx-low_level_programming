29cc5ce51499018d1b7cb8bacdd2d2a8283fdfd8
Ngcali <magopeningcali@gmail.com>
2022-12-25 14:06:35 -0800

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,11 +11,10 @@ int main(void)
 	int i = 0;
 
 	printf("Infinite loop incoming :(\n");
-	/**
-	 * while (i < 10)
-	 * {
-	 * 	putchar(i);
-	 * }
+	/*while (i < 10)
+	{
+		putchar(i);
+	}*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
