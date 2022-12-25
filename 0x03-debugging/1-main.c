82358bff86e90ff7fdc19732ae59833f4734391b
Ngcali <magopeningcali@gmail.com>
2022-12-25 13:51:59 -0800

    1. Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
 
 /**
- * * main - causes an infinite loop
- * * Return: 0
- * */
+ * main - causes an infinite loop.
+ *
+ * Return: 0
+ */
 
 int main(void)
 {
-    int i;
+    int i = 0;
     
     printf("Infinite loop incoming :(\n");
-    
-    i = 0;
     
     /*while (i < 10)
     {
@@ -20,5 +19,5 @@ int main(void)
 
     printf("Infinite loop avoided! \\o/\n");
     
-    return(0);
+    return (0);
 }
