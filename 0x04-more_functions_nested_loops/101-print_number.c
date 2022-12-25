a1ebf1db8bb5065faa8ff68c9fe760ecc273710e
Ngcali <magopeningcali@gmail.com>
2022-12-25 12:38:32 -0800

    12. Numbers have life; they're not just symbols on paper

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -15,14 +15,11 @@ void print_number(int n)
 	if (n < 0)
 	{
 		n = n * -1;
-		i = n;
 		_putchar('-');
 	}
 
-	i = i / 10;
-
-	if (i != 0)
-		print_number(i);
+	if (n / 10)
+		print_number(n / 10);
 
 	_putchar((unsigned int) n / 10 + '0');
 }
