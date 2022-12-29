e1e3f882d37af6d5bf6f6dc062ce24f0862b6a78
Ngcali <magopeningcali@gmail.com>
2022-12-29 09:06:09 -0800

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,14 @@
 #include "main.h"
 
 /**
- * reset_to_9 - takes a pointer to
+ * reset_to_98 - takes a pointer to
  * an int as parameter and updates
  * the value it points to to 98.
  *
  * @*n: pointer which points to n.
+ *
+ * @n: an integer in which a pointer
+ * will be taken to.
  */
 
 void reset_to_98(int *n)
