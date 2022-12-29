3b844b413d90106f704c63351629bb4f8f5c3594
Ngcali <magopeningcali@gmail.com>
2022-12-29 12:46:21 -0800

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,18 @@
 #include "main.h"
 
+/**
+ * _strlen - returns the length
+ * of a string.
+ *
+ * @*s: pointer which points to
+ * s.
+ *
+ * @s: string of which length is
+ * returned.
+ *
+ * REturn: length of a string.
+ */
+
 int _strlen(char *s)
 {
 	int a;
