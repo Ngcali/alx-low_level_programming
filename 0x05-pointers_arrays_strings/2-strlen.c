fe9863e6e240ddb7e0bfe21f635f7ce77205b3f6
Ngcali <magopeningcali@gmail.com>
2022-12-29 12:42:24 -0800

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,10 +2,12 @@
 
 int _strlen(char *s)
 {
-	char my_str_array[] = *s;
 	int a;
 
-	for (a = 0; my_str_array[a] != '\0'; a++)
-	
-	printf(a);
+	for (a = 0; *s != '\0'; s++)
+	{
+		a++;
+	}
+
+	return (a);
 }
