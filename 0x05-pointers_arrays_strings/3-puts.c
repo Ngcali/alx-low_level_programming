de63abedab004660258275d567b48d2d51788aa7
Ngcali <magopeningcali@gmail.com>
2022-12-31 12:52:11 -0800

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,18 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * _puts - prints a string
+ * followed by a new line.
+ *
+ * @*str: pointer which points
+ * to str.
+ *
+ * @str: sting in which pointer
+ * points to.
+ */
+
 void _puts(char *str)
 {
-	printf("%s\n", str);
+	_puts(*str);
 }
