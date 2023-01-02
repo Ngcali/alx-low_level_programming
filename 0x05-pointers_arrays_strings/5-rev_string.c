78ec2ca154dde12de2e12d46e2a3e64ad74d181c
Ngcali <magopeningcali@gmail.com>
2023-01-02 03:55:33 -0800

    5. A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,6 +1,17 @@
 #include "main.h"
 #include "stdio.h"
 
+/**
+ * rev_string - reverses a string.
+ *
+ * @*s: pointer which points to
+ * char s.
+ *
+ * @s: char which is pointed to.
+ *
+ * Return: void.
+ */
+
 void rev_string(char *s)
 {
 	int i;
@@ -11,7 +22,7 @@ void rev_string(char *s)
 	{}
 
 	my_counter = 0;
-	while(my_counter < i)
+	while (my_counter < i)
 	{
 		my_temp_var = s[my_counter];
 		s[my_counter] = s[i - my_counter - 1];
