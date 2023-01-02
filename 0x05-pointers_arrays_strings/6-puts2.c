d711f566c682ad075c74c48ee6b37803d08f58cd
Ngcali <magopeningcali@gmail.com>
2023-01-02 05:04:23 -0800

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,22 @@
 #include "main.h"
 #include "stdio.h"
 
+/**
+ * puts2 - prints every other
+ * character of a string,
+ * starting with the first
+ * character, followed by a
+ * new line.
+ *
+ * @*str: pointer that points
+ * to char str.
+ *
+ * @str: string that will be
+ * printed.
+ *
+ * Return: void
+ */
+
 void puts2(char *str)
 {
 	int i;
