2ceb66ab936d5830dc395ef46c2e18f38427713b
Ngcali <magopeningcali@gmail.com>
2023-01-02 06:29:46 -0800

    7. Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -5,11 +5,12 @@ void puts_half(char *str)
 {
 	int i;
 	int my_num = 0;
-	char n = (length_of_the_string - 1) / 2;
 
 	for (i = 0; str[i] != '\0'; ++i)
 	{}
 
+	length_of_the_string = i;
+	char n = (length_of_the_string - 1) / 2;
 	if (i % 2 == 0)
 	{
 		while (my_num < i)
