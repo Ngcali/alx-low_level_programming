66b2602bfe680a2598e26a9f6ca6addf4dafe065
Ngcali <magopeningcali@gmail.com>
2023-01-02 03:45:49 -0800

    5. A good engineer thinks in reverse and asks himself about the stylistic consequences of the components and systems he proposes

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include "stdio.h"
+
+void rev_string(char *s)
+{
+	int i;
+	int my_counter;
+	int my_temp_var;
+
+	for (i = 0; s[i] != '\0'; ++i)
+	{}
+
+	my_counter = 0;
+	while(my_counter < i)
+	{
+		my_temp_var = s[my_counter]
+		s[my_counter] = s[i - my_counter - 1];
+		s[i - my_counter - 1] = my_temp_var;
+		my_counter++;
+	}
+}
