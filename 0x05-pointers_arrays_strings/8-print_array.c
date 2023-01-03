3072c86a140fad6c80476cfb948af06a4a65ee55
Ngcali <magopeningcali@gmail.com>
2023-01-02 23:15:49 -0800

    8. Arrays are not pointers

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -25,13 +25,13 @@ void print_array(int *a, int n)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i < n)
-		{
-			printf("%d, ", a[i]);
-		}
-		else if (i == n - 1)
+		if (i == (n - 1))
 		{
 			printf("%d", a[i]);
+		}
+		else if (i < n)
+		{
+			printf("%d, ", a[i]);
 		}
 
 	}
