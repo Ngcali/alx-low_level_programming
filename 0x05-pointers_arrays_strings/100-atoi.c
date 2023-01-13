4f03ae60187a6f4dc0ae344faec25f824bdaa129
Ngcali <magopeningcali@gmail.com>
2023-01-13 09:14:31 -0800

    10. Great leaders are willing to sacrifice the numbers to save the people. Poor leaders sacrifice the people to save the numbers

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,15 +5,17 @@ int _atoi(char *s)
 {
 	int i;
 	int my_number = 0;
-	int my_sign = 1;
+	/*int my_sign = 1;*/
 
 	for (i = 0; s[i] != '\0'; ++i)
 	{
-		if (str[i]> '9' || str[i]<'0')
+		if (s[i]> '9' || s[i]<'0')
 		{
 			return (-1);
 		}
+
+		my_number = mynumber * 10 + s[i] - '0';
 	}
 
-	/*return (my_number);*/
+	return (my_number);
 }
