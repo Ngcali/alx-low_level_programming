76ed0dab2d526f3300ba5f52db71da7e15f845fb
Ngcali <magopeningcali@gmail.com>
2023-01-16 03:33:38 -0800

    11. Don't hate the hacker, hate the code

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -6,7 +6,9 @@ int main(void)
 	int k;
 	int my_ascii = 2772;
 	char my_password[100];
+	time_t my_time;
 
+	srand((int) time(&my_time));
 	while (my_ascii > 126)
 	{
 		my_password[i] = rand() % 126;
