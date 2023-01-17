609f6cb7a6b6f32f1647da118983964e332d3401
Ngcali <magopeningcali@gmail.com>
2023-01-17 06:45:02 -0800

    5. Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,17 +1,27 @@
 #include "main.h"
 
+/**
+ * string_toupper - changes all lowercase letters
+ * of a string to uppercase.
+ *
+ * @*str: pointer to str.
+ *
+ * @str: string of which letters will be changed
+ * to uppercase.
+ *
+ * Return: string with uppercase letters.
+ */
+
 char *string_toupper(char *str)
 {
-	int a = 0;
+	int i;
 
-	while (str[a])
+	for (i = 0; i <= str[i]; i++)
 	{
-		if (str[a] >= 97 && str[a] <= 122)
+		if (str[i] >= 97 && str[i] <= 122)
 		{
-			str[a] -= 32;
+			str[i] -= 32;
 		}
-
-		a++;
 	}
 
 	return (str);
