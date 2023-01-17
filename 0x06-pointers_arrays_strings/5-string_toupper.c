9413daa5762d3d5e72480145cc1c54ba5b4f29a9
Ngcali <magopeningcali@gmail.com>
2023-01-17 06:32:47 -0800

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+char *string_toupper(char *str)
+{
+	int a = 0;
+
+	while (str[a])
+	{
+		if (str[a] >= 97 && str[a] <= 122)
+		{
+			str[a] -= 32;
+		}
+
+		a++;
+	}
+
+	return (str);
+}
