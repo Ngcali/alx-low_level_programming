0d2a20343a67674ec9fcd8c848297a78020fa69f
Ngcali <magopeningcali@gmail.com>
2023-01-16 22:02:15 -0800

    3. strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,60 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings.
+ *
+ * @*s1: pointer to s1.
+ * @*s2: pointer to s2.
+ *
+ * @s1: first string to compare.
+ * @s2: second string to compare.
+ *
+ * Return: integer.
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int x = 0;
+	int y = 0;
+	int z = 0;
+	int j = 0;
+	int my_lim;
+
+	while (s1[x])
+	{
+		x++;
+	}
+
+	while (s2[y])
+	{
+		y++;
+	}
+
+	if (x <= y)
+	{
+		my_lim = x;
+	}
+
+	else
+	{
+		my_lim = y;
+	}
+
+	while (z <= my_lim)
+	{
+		if (s1[z] == s2[z])
+		{
+			z++;
+			continue;
+		}
+		else
+		{
+			j = s1[z] - s2[z];
+			break;
+		}
+
+		z++;
+	}
+
+	return (j);
+}
