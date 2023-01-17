a243459c62603a8b3a60d9067dc7a243b1f69ed7
Ngcali <magopeningcali@gmail.com>
2023-01-16 20:49:05 -0800

    1. strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings.
+ *
+ * @*dest: pointer to dest.
+ * @*src: pointer to src.
+ *
+ * @dest: string to concatenate to.
+ * @src: string to be cocatenated.
+ * @n: limit of concatenation.
+ *
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_length = 0;
+	int i;
+
+	while (dest[dest_length])
+	{
+		dest_len++;
+	}
+
+	for (i = 0; i < n && src[j]; i++)
+	{
+		dest[dest_length] = src[i];
+		dest_length++;
+	}
+
+	dest[dest_length + n + 1] = '\0';
+	return (dest);
+}
