99a14ca4e5f733f94282d265baaae23061c3ef75
Ngcali <magopeningcali@gmail.com>
2023-01-21 01:00:20 -0800

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encodes a string using rot13.
+ *
+ * @*str: pointer to str.
+ *
+ * @str: string to be encoded.
+ *
+ * Return: encoded string.
+ */
+
+char *rot13(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		switch (str[i])
+		{
+			case 'a'...'m': case 'A'...'M':
+				str[i] += 13;
+				break;
+			case 'n'...'z': case 'N'...'Z':
+				str[i] -= 13;
+				break;
+			default:
+				break;
+		}
+	} i++;
+
+	return (str);
+}
