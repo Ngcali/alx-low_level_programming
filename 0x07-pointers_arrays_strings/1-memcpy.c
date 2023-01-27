0a75dc217bd88ce8d5b506f71e21f4a986f6176f
Ngcali <magopeningcali@gmail.com>
2023-01-26 16:56:51 -0800

    1. memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+#include "main.h"
+
+/**
+ * _memcpy - that copies memory area.
+ *
+ * @*dest: ponter to char dest.
+ * @*src: pointer to char src.
+ *
+ * @dest: destination memory area where source
+ * memory area will be copied to.
+ * @src: source memory area to be copied.
+ *
+ * Return: a pointer to dest.
+ */
+
+char *_memcpy(char *dest, char src, unsigned int n)
+{
+	unsigned int my_int;
+
+	my_int = 0;
+	while (my_int < n)
+	{
+		dest[my_int] = src[my_int];
+		my_int++;
+	}
+
+	return (dest);
+}
