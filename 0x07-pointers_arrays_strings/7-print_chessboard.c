26183a765d8dcda481ab1607e4b7205f3fa96ac8
Ngcali <magopeningcali@gmail.com>
2023-01-28 04:44:08 -0800

    6. Chess is mental torture

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard.
+ *
+ * @*a: pointer to char a.
+ *
+ * @a: an array of chess pieces.
+ *
+ * Return: Nothing.
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int counter_one, counter_two;
+
+	counter_one = 0;
+	counter_two = 0;
+
+	while (counter_one < 8)
+	{
+		while (counter_two < 8)
+		{
+			_putchar(a[counter_one][counter_two]);
+			counter_two++;
+		}
+
+		_putchar('\n');
+		counter_one++;
+	}
+}
