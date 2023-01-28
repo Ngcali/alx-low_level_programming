14093486b908f7d4f831e9eefb54f0c5a82d8839
Ngcali <magopeningcali@gmail.com>
2023-01-28 04:17:46 -0800

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring.
+ *
+ * @*haystac: pointer to char haystac.
+ * @*needle: pointer to char needle.
+ *
+ * @haystack: string where substring
+ * will be located.
+ * @needle: string to locate.
+ *
+ * Return: pointer to the beginning
+ * of the located substring, or NULL
+ * if the substring is not found.
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int counter_one, counter_two;
+
+	for (counter_one = 0; haystack[0]; counter_one++)
+	{
+		for (counter_two = 0; needle[counter_two]; counter_two++)
+			if (haystack[counter_one + counter_two] != needle[counter_two])
+				break;
+
+		if (needle[counter_two] == '\0')
+			return (haystack + counter_one)
+	}
+
+	return ('\0);
+}
