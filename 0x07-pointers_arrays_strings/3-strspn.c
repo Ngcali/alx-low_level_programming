1ff94ebc3a86b8a92d80ef87b058b22f0a632162
Ngcali <magopeningcali@gmail.com>
2023-01-28 01:47:54 -0800

    3. strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring.
+ *
+ * @*s: pointer to char s.
+ * @*accept: pointer to char accept.
+ *
+ * @s: source string.
+ * @accept: string that will be accepted.
+ *
+ * Return: the number of bytes in the initial
+ * segment of s which consist only of bytes
+ * from accept.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int counter_one, counter_two;
+
+	my_bytes = 0;
+
+	for (counter_one = 0; accept[0]; counter_one++)
+	{
+		for (counter_two; s[counter_two] != 32; counter_two++)
+		{
+			if (accept[counter_one] == s[counter_two])
+			{
+				my_bytes++;
+			}
+		}
+	}
+
+	return (my_bytes);
+}
