5fb918983c5846dbd4a1ab2b02cf6c45f6bbda51
Ngcali <magopeningcali@gmail.com>
2023-01-31 04:51:50 -0800

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,7 +16,7 @@ int check_if_prime(int n, int i);
 
 int is_prime_number(int n)
 {
-	return (check_prime(n, 1));
+	return (check_if_prime(n, 1));
 }
 
 /**
@@ -40,5 +40,5 @@ int check_if_prime(int n, int i)
 	if ((n / i) < i)
 		return (1);
 
-	return (check_prime(n, i + 1));
+	return (check_if_prime(n, i + 1));
 }
