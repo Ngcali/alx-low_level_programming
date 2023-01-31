59c2bd033fd63c8483f1c5a64644d77b0e305922
Ngcali <magopeningcali@gmail.com>
2023-01-30 16:19:00 -0800

    1. Why is it so important to dream? Because, in my dreams we are together

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -14,9 +14,11 @@ void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*s);
+		return;
 	}
 
-	return;
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
 }
