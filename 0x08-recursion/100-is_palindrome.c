a55c05090eb0798d89898f21f3546fc4938b27b5
Ngcali <magopeningcali@gmail.com>
2023-02-02 02:58:14 -0800

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,71 +1,28 @@
-#include "main.h"
-
-int check_if_palindrome(char *s);
-
-/**
- * is_palindrome - returns 1 if a string
- * is a palindrome and 0 if not.
- *
- * @*s: pointer to char s.
- *
- * @s: string to be checked if it is a
- * palindrome.
- *
- * Return: interger value.
- */
-
+#include <stdio.h>
+#include <string.h>
+ 
+void check(char [], int);
+ 
 int is_palindrome(char *s)
 {
-	if (*s == '0')
-		return (1);
-
-	return (check_if_palindrome(s));
+    check_if_palindrome(s, 0);
+ 
+    return 0;
 }
-
-/**
- * check_if_palindrome - checkes if a
- * string is palindrome.
- *
- * @s: string to be checked if it is
- * a palindrome.
- *
- * Return: integer value.
- */
-
-int check_if_palindrome(char *s)
+ 
+int check_if_palindrome(char s[], int index)
 {
-	int str_l = _strlen_recursion(s) - 1;
-
-	if (*s == s[str_l])
-	{
-		s++;
-		str_l--;
-	}
-	else
-	{
-		return (0);
-	}
-
-	return (1);
-}
-
-/**
- * _strlen_recursion - calculates the
- * length of a string.
- *
- * @s: string of which length is to be
- * calculated.
- *
- * Return: integer value.
- */
-
-int _strlen_recursion(char *s)
-{
-	if (*s == '\0')
-	{
-		return (0);
-	}
-
-	s++;
-	return (_strlen_recursion(s) + 1);
+    int len = strlen(s) - (index + 1);
+    if (s[index] == s[len])
+    {
+        if (index + 1 == len || index == len)
+        {
+            return (1);
+        }
+        check_if_palindrome(s, index + 1);
+    }
+    else
+    {
+      return (0);
+    }
 }
