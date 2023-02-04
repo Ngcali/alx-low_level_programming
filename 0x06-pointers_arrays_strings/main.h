0d9bc029606f295487f9003f50f913e473cd2957
Ngcali <magopeningcali@gmail.com>
2023-02-04 11:55:05 -0800

    Added main.h file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,3 +6,6 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *str);
 char *cap_string(char *str);
 char *leet(char *str);
+void print_number(int n);
+int _putchar (int n);
+void print_number(int n);
