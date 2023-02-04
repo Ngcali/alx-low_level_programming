125dd5fd1af9abcab1c57b9c75947672925decc3
Ngcali <magopeningcali@gmail.com>
2023-02-04 05:18:55 -0800

    7. They say we only use a fraction of our brain's true potential. Now that's when we're awake. When we're asleep, we can do almost anything

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,15 +3,8 @@
 #include <stdio.h>
 
 /**
- * is_palindrome - returns 1 if a
- * string is a palindrome and 0 if not.
- *
  * is_palindrome_helper - where the
  * recursive logic is implemented.
- *
- * @*s: is a pointer to char s.
- *
- * @s: is the string to be checked.
  *
  * @start: start index of the current
  * substring being checked for palindromeness.
@@ -28,10 +21,21 @@ int is_palindrome_helper(char *s, int start, int end)
 		return 1;
 	if (s[start] != s[end])
 		return 0;
-	return is_palindrome_helper(s, start + 1, end - 1);
+	return (is_palindrome_helper(s, start + 1, end - 1));
 }
+
+/**
+ * is_palindrome - returns 1 if a
+ * string is a palindrome and 0 if not.
+ *
+ * @*s: is a pointer to char s.
+ *
+ * @s: is the string to be checked.
+ *
+ * Return: interger value
+ */
 
 int is_palindrome(char *s)
 {
-	return is_palindrome_helper(s, 0, strlen(s) - 1);
+	return (is_palindrome_helper(s, 0, strlen(s) - 1));
 }
