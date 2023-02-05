d29d7e37d8936ce64e25ca36f307b63ff2eba994
Ngcali <magopeningcali@gmail.com>
2023-02-05 07:22:31 -0800

    12. Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -13,23 +13,26 @@
  * Return: Nothing
  */
 
-void print_buffer(char *b, int size) {
-    if (size <= 0) {
-        printf("\n");
-        return;
-    }
+void print_buffer(char *b, int size)
+{
+	int i;
 
-    for (int i = 0; i < size; i++) {
-        if (i % 10 == 0)
-            printf("%08x: ", i);
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
 
-        printf("%02x ", b[i]);
-
-        if (i % 10 == 9 || i == size - 1) {
-            for (int j = i - (i % 10); j <= i; j++)
-                printf("%c", isprint(b[j]) ? b[j] : '.');
-
-            printf("\n");
-        }
-    }
+	for (i = 0; i < size; i++)
+	{
+		if (i % 10 == 0)
+			printf("%08x: ", i);
+		printf("%02x ", b[i]);
+		if (i % 10 == 9 || i == size - 1)
+		{
+			for (int j = i - (i % 10); j <= i; j++)
+				printf("%c", isprint(b[j]) ? b[j] : '.');
+			printf("\n");
+		}
+	}
 }
