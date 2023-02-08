9ada3194429f6eedf3f53800b2ca43ad15f1e98e
Ngcali <magopeningcali@gmail.com>
2023-02-08 06:00:36 -0800

    0. _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -10,15 +10,16 @@
  * Return: C program always returns 0.
  */
 
-int _putchar(void) {
-  putchar('_');
-  putchar('p');
-  putchar('u');
-  putchar('t');
-  putchar('c');
-  putchar('h');
-  putchar('a');
-  putchar('r');
-  putchar('\n');
-  return (0);
+int _putchar(char c)
+{
+        c[10] = "_putchar";
+	int my_number = 0;
+
+	while (my_number < 8)
+	{
+		_putchar(c[my_number]);
+		my_number++;
+	}
+	_putchar('\n');
+	return (0);
 }
