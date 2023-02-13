a3c91eb34d3b1fb4b6e94f0039ee93dfc40d3d57
Ngcali <magopeningcali@gmail.com>
2023-02-13 00:47:18 -0800

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,8 +1,20 @@
+#include "main.h"
 #include <stdio.h>
-#include <unistd.h>
 
-int main(int argc, char *argv[])
+/**
+ * main - prints program name,
+ * followed by a new line.
+ *
+ * @*argv: pointer to char argv.
+ *
+ * @argc: number of arguments.
+ * @argv: array of arguments.
+ *
+ * Return: integer value.
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
-	return 0;
+	return (0);
 }
