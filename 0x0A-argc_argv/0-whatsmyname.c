ce2f87d68da00af3a24cebdbff2b75bbb2b18b44
Ngcali <magopeningcali@gmail.com>
2023-02-12 23:04:55 -0800

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,7 +1,8 @@
+#include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, char *argv[]) {
-  printf("%s\n", argv[0]);
-  return 0;
+int main(int argc, char *argv[])
+{
+	printf("argc = %d\n", argc);
+	return 0;
 }
-
