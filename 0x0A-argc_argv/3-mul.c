b6802c90c4040e72931080543c12a24649d397aa
Ngcali <magopeningcali@gmail.com>
2023-02-18 01:26:24 -0800

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,18 +13,21 @@
  * Return: 0
  */
 
-int main(int argc, char *argv[]) {
-    int my_num1, my_num2, my_result;
+int main(int argc, char *argv[])
+{
+	int my_num1, my_num2, my_result;
 
-    if (argc != 3) {
-        printf("Error\n");
-        return 1;
-    }
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
 
-    my_num1 = atoi(argv[1]);
-    my_num2 = atoi(argv[2]);
-    my_result = my_num1 * my_num2;
+	my_num1 = atoi(argv[1]);
+	my_num2 = atoi(argv[2]);
+	my_result = my_num1 * my_num2;
 
-    printf("%d\n", my_result);
-    return 0;
+	printf("%d\n", my_result);
+
+	return (0);
 }
