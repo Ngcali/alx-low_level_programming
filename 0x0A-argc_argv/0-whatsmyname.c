4044ec340f1348169865c8b8d8ee259482c69d8e
Ngcali <magopeningcali@gmail.com>
2023-02-17 23:36:46 -0800

    0. It ain't what they call you, it's what you answer to

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -17,9 +17,9 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Usage: %s <argument>\n", argv[0]);
-		return 1;
+		return (1);
 	}
 
 	printf("The argument is: %s\n", argv[1]);
-	return 0;
+	return (0);
 }
