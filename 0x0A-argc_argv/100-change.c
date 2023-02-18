f3ab06c52e069c5f5cc68e84a18fdc4582db1fe4
Ngcali <magopeningcali@gmail.com>
2023-02-18 02:05:57 -0800

    5. Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -21,16 +21,20 @@ int main(int argc, char *argv[])
 	int cents, coins = 0;
 
 	if (argc != 2)
+	{
 		printf("Error\n");
 
 		return 1;
+	}
 
 	cents = atoi(argv[1]);
 
 	if (cents < 0)
+	{
 		printf("0\n");
 
 		return 0;
+	}
 
 	while (cents > 0)
 	{
