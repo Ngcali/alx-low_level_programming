40e84700564e17495fc539867849a3ba71da4082
Ngcali <magopeningcali@gmail.com>
2023-03-18 08:33:02 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,25 +11,25 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-    size_t s1_len, s2_len;
-    char *result;
+	size_t s1_len, s2_len;
+	char *result;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-    s1_len = strlen(s1);
-    s2_len = strlen(s2);
+	s1_len = strlen(s1);
+	s2_len = strlen(s2);
 
-    result = malloc((s1_len + s2_len + 1) * sizeof(char));
-    if (result == NULL)
-        return (NULL);
+	result = malloc((s1_len + s2_len + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
 
-    memcpy(result, s1, s1_len);
-    memcpy(result + s1_len, s2, s2_len);
+	memcpy(result, s1, s1_len);
+	memcpy(result + s1_len, s2, s2_len);
 
-    result[s1_len + s2_len] = '\0';
+	result[s1_len + s2_len] = '\0';
 
-    return (result);
+	return (result);
 }
