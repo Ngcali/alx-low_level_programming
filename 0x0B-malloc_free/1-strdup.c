3fc183e41eec3e82a0b0b472e4115b6a8b94b11e
Ngcali <magopeningcali@gmail.com>
2023-03-18 08:21:10 -0700

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -17,18 +17,18 @@
 
 char *_strdup(char *str)
 {
-    char *duplicate_str;
-    size_t str_len;
+	char *duplicate_str;
+	size_t str_len;
 
-    if (str == NULL)
-        return (NULL);
+	if (str == NULL)
+		return (NULL);
 
-    str_len = strlen(str);
-    duplicate_str = malloc(sizeof(char) * (str_len + 1));
-    if (duplicate_str == NULL)
-        return (NULL);
+	str_len = strlen(str);
+	duplicate_str = malloc(sizeof(char) * (str_len + 1));
+	if (duplicate_str == NULL)
+		return (NULL);
 
-    strcpy(duplicate_str, str);
+	strcpy(duplicate_str, str);
 
-    return (duplicate_str);
+	return (duplicate_str);
 }
