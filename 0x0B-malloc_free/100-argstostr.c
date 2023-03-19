f573d5bbfef12a3c654f5abc222ec01a8ab8d7d2
Ngcali <magopeningcali@gmail.com>
2023-03-19 01:40:15 -0700

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -5,48 +5,50 @@
  * @ac: argument count
  * @av: argument vector
  *
- * Return: pointer to a new string containing all the arguments concatenated
- *         followed by a newline, or NULL if ac == 0 or av == NULL or malloc
- *         fails
+ * Return: pointer to a new string containing all
+ * the arguments concatenated
+ * followed by a newline, or NULL if ac == 0 or av == NULL or malloc
+ * fails
  */
 char *argstostr(int ac, char **av)
 {
-        int i, j, len = 0, total_length = 0;
-        char *result;
+	int i, j, len = 0, total_length = 0;
+	char *result;
 
-        if (ac == 0 || av == NULL)
-                return (NULL);
+	if (ac == 0 || av == NULL)
+		return (NULL);
 
-        for (i = 0; i < ac; i++)
-        {
-                len = 0;
-                while (av[i][len])
-                {
-                        len++;
-                        total_length++;
-                }
-                total_length++;
-        }
+	for (i = 0; i < ac; i++)
+	{
+		len = 0;
 
-        result = malloc((total_length + 1) * sizeof(char));
-        if (result == NULL)
-                return (NULL);
+		while (av[i][len])
+		{
+			len++;
+			total_length++;
+		}
+		total_length++;
+	}
 
-        total_length = 0;
-        for (i = 0; i < ac; i++)
-        {
-                j = 0;
-                while (av[i][j])
-                {
-                        result[total_length] = av[i][j];
-                        j++;
-                        total_length++;
-                }
-                result[total_length] = '\n';
-                total_length++;
-        }
-        result[total_length] = '\0';
+	result = malloc((total_length + 1) * sizeof(char));
+	if (result == NULL)
+		return (NULL);
 
-        return (result);
+	total_length = 0;
+	for (i = 0; i < ac; i++)
+	{
+		j = 0;
+
+		while (av[i][j])
+		{
+			result[total_length] = av[i][j];
+			j++;
+			total_length++;
+		}
+		result[total_length] = '\n';
+		total_length++;
+	}
+	result[total_length] = '\0';
+
+	return (result);
 }
-
