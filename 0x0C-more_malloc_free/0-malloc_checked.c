cab74f0fd1abbbde00e9218ccbc97d63c8b656bc
Ngcali <magopeningcali@gmail.com>
2023-03-19 05:06:55 -0700

    0. Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,10 +1,20 @@
-#include <stdlib.h> // for malloc, exit
+#include <stdlib.h>
 
-void *malloc_checked(unsigned int b) {
-  void *p = malloc(b);
-  if (!p) {
-    exit(98);
-  }
-  return p;
+/**
+ * malloc_checked - Allocates memory using malloc
+ * @b: The number of bytes to allocate
+ *
+ * Return: A pointer to the allocated memory
+ *         If malloc fails, the function terminates the process with a status
+ *         value of 98
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (!ptr)
+		exit(98);
+
+	return (ptr);
 }
 
