b28d549b9d56fddd4fd80cac423d38977ec7518e
Ngcali <magopeningcali@gmail.com>
2023-03-21 04:19:04 -0700

    3. array_range

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+
+int* array_range(int min, int max) {
+    if (min > max) {
+        return NULL;
+    }
+
+    int* arr = malloc((max - min + 1) * sizeof(int));
+    if (arr == NULL) {
+        return NULL;
+    }
+
+    for (int i = 0; i <= max - min; i++) {
+        arr[i] = min + i;
+    }
+
+    return arr;
+}
