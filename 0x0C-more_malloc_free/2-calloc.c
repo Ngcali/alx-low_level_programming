d22de18c4ce4ad13ee14b0450bdd5bbc5d07b449
Ngcali <magopeningcali@gmail.com>
2023-03-21 02:08:14 -0700

    2. _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+
+void *_calloc(unsigned int nmemb, unsigned int size) {
+    void *ptr;
+    
+    if (nmemb == 0 || size == 0) {
+        return NULL;
+    }
+    
+    ptr = malloc(nmemb * size);
+    if (ptr == NULL) {
+        return NULL;
+    }
+    
+    // set memory to zero
+    memset(ptr, 0, nmemb * size);
+    
+    return ptr;
+}
