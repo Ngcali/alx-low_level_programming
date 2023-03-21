773c6b0e618f5c644920076e5899975ddc297aa3
Ngcali <magopeningcali@gmail.com>
2023-03-21 04:52:53 -0700

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -3,26 +3,22 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size) {
         free(ptr);
         return NULL;
     }
-
+    
     if (ptr == NULL) {
         return malloc(new_size);
     }
-
+    
     if (new_size == old_size) {
         return ptr;
     }
-
+    
     void *new_ptr = malloc(new_size);
-
-    if (new_ptr == NULL) {
-        return NULL;
-    }
-
-    // Copy contents of old memory block to new memory block
-    unsigned int min_size = old_size < new_size ? old_size : new_size;
-    memcpy(new_ptr, ptr, min_size);
-
+    
+    unsigned int copy_size = old_size < new_size ? old_size : new_size;
+    
+    memcpy(new_ptr, ptr, copy_size);
+    
     free(ptr);
-
+    
     return new_ptr;
 }
