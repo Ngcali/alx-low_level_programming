dcf29ed1156fd03d3d78c46b59294b69e0443801
Ngcali <magopeningcali@gmail.com>
2023-03-21 07:49:45 -0700

    4. _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,47 +1,48 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * _realloc - Reallocates a memory block using malloc and free
- * @ptr: Pointer to the memory previously allocated
- * @old_size: Size in bytes of the allocated space for ptr
- * @new_size: Size in bytes for the new memory block
+ * _realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previously allocated
+ * @old_size: size of the allocated space for ptr
+ * @new_size: size of the new memory block
  *
- * Return: Pointer to the reallocated memory block
+ * Return: pointer to the new allocated memory block
+ *         NULL if new_size is 0 and ptr is not NULL
+ *         ptr if new_size is equal to old_size
+ *         NULL if malloc fails
  */
-
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	void *new_ptr;
+    char *new_ptr, *old_ptr;
+    unsigned int i;
 
-	if (new_size == old_size)
-		return (ptr);
+    if (new_size == 0 && ptr != NULL)
+    {
+        free(ptr);
+        return (NULL);
+    }
 
-	if (new_size == 0)
-	{
-		free(ptr);
-		return (NULL);
-	}
+    if (new_size == old_size)
+        return (ptr);
 
-	if (ptr == NULL)
-	{
-		new_ptr = malloc(new_size);
-		if (new_ptr == NULL)
-			return (NULL);
-		return (new_ptr);
-	}
+    if (ptr == NULL)
+    {
+        new_ptr = malloc(new_size);
+        if (new_ptr == NULL)
+            return (NULL);
+        return (new_ptr);
+    }
 
-	new_ptr = malloc(new_size);
-	if (new_ptr == NULL)
-		return (NULL);
+    new_ptr = malloc(new_size);
+    if (new_ptr == NULL)
+        return (NULL);
 
-	if (new_size > old_size)
-		memcpy(new_ptr, ptr, old_size);
-	else
-		memcpy(new_ptr, ptr, new_size);
+    old_ptr = ptr;
+    for (i = 0; i < old_size && i < new_size; i++)
+        new_ptr[i] = old_ptr[i];
 
-	free(ptr);
+    free(ptr);
 
-	return (new_ptr);
+    return (new_ptr);
 }
