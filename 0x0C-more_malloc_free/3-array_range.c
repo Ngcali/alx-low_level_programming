020c83807519bbea070d300d4e13f7c670a8da13
Ngcali <magopeningcali@gmail.com>
2023-03-21 04:36:43 -0700

    3. array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -1,6 +1,16 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * array_range - creates an array of integers
+ *
+ * @min: minimum value to include in the array
+ * @max: maximum value to include in the array
+ *
+ * Return: pointer to the newly created array,
+ * or NULL if an error occurs
+ */
+
 int *array_range(int min, int max)
 {
 	int i;
