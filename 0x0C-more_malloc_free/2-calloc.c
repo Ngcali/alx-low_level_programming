5812917255d5d7e0e74f1c0b0eb04427a55ed426
Ngcali <magopeningcali@gmail.com>
2023-03-21 02:11:14 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,19 +1,25 @@
 #include <stdlib.h>
+#include <string.h>
 
-void *_calloc(unsigned int nmemb, unsigned int size) {
+/**
+ * _calloc - allocate memory for an array of nmemb elements of size bytes each
+ * @nmemb: number of elements to allocate memory for
+ * @size: size of each element in bytes
+ *
+ * Return: pointer to allocated memory or NULL if allocation failed
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
     void *ptr;
-    
-    if (nmemb == 0 || size == 0) {
+
+    if (nmemb == 0 || size == 0)
         return NULL;
-    }
-    
+
     ptr = malloc(nmemb * size);
-    if (ptr == NULL) {
+    if (ptr == NULL)
         return NULL;
-    }
-    
-    // set memory to zero
+
     memset(ptr, 0, nmemb * size);
-    
+
     return ptr;
 }
