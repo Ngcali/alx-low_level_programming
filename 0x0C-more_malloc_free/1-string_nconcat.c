58567e2d91f69a7a10e745b20ce9c7ec683386c5
Ngcali <magopeningcali@gmail.com>
2023-03-21 02:00:45 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,27 +12,27 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *result;
-    unsigned int i, j;
-    size_t len1 = 0, len2 = 0;
+	char *result;
+	unsigned int i, j;
+	size_t len1 = 0, len2 = 0;
 
-    if (s1 != NULL)
-        len1 = strlen(s1);
-    if (s2 != NULL)
-        len2 = strlen(s2);
+	if (s1 != NULL)
+		len1 = strlen(s1);
+	if (s2 != NULL)
+		len2 = strlen(s2);
 
-    if (n >= len2)
-        n = len2;
+	if (n >= len2)
+		n = len2;
 
-    result = malloc(sizeof(char) * (len1 + n + 1));
-    if (result == NULL)
-        return (NULL);
+	result = malloc(sizeof(char) * (len1 + n + 1));
+	if (result == NULL)
+		return (NULL);
 
-    for (i = 0; i < len1; i++)
-        result[i] = s1[i];
-    for (j = 0; j < n; j++, i++)
-        result[i] = s2[j];
-    result[i] = '\0';
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < n; j++, i++)
+		result[i] = s2[j];
+	result[i] = '\0';
 
-    return (result);
+	return (result);
 }
