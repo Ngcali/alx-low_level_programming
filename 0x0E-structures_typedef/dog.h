a9676cff75536b39b0c2ef6cf3ffa0fb76c382f7
Ngcali <magopeningcali@gmail.com>
2023-03-22 07:15:31 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,20 @@
-struct dog {
-  char *name;
-  float age;
-  char *owner;
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a structure representing a dog
+ *
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
+
+#endif /* DOG_H */
 
