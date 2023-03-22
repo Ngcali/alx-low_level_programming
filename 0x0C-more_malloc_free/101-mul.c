7768e6f22702628697373092885ee0282916ec21
Ngcali <magopeningcali@gmail.com>
2023-03-22 05:51:10 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -4,10 +4,10 @@
 #include <ctype.h>
 
 /**
- * is_valid_number - Checks if a string represents a valid positive number
+ * is_valid_number - Check if a string represents a valid positive number
  * @num: The string to check
  *
- * Return: 1 if the string represents a valid positive number, 0 otherwise
+ * Return: 1 if @num is a valid positive number, 0 otherwise
  */
 int is_valid_number(char *num)
 {
@@ -23,14 +23,15 @@ int is_valid_number(char *num)
 }
 
 /**
- * str_to_int - Converts a string to an integer
+ * str_to_int - Convert a string to an integer
  * @num: The string to convert
  *
- * Return: The integer value of the string
+ * Return: The integer value of @num
  */
 int str_to_int(char *num)
 {
     int result = 0;
+
     while (*num)
     {
         result = result * 10 + (*num - '0');
@@ -40,23 +41,25 @@ int str_to_int(char *num)
 }
 
 /**
- * main - Entry point. Multiplies two positive numbers passed as arguments.
- * @argc: The number of arguments
- * @argv: The argument vector
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
  *
  * Return: 0 on success, 98 on error
  */
 int main(int argc, char *argv[])
 {
+    int num1, num2, result;
+
     if (argc != 3 || !is_valid_number(argv[1]) || !is_valid_number(argv[2]))
     {
         printf("Error\n");
-        exit(98);
+        return 98;
     }
 
-    int num1 = str_to_int(argv[1]);
-    int num2 = str_to_int(argv[2]);
-    int result = num1 * num2;
+    num1 = str_to_int(argv[1]);
+    num2 = str_to_int(argv[2]);
+    result = num1 * num2;
 
     printf("%d\n", result);
     return 0;
