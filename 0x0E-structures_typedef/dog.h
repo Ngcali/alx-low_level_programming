ece421e1acc1b00f9b737eda5a802bfb008c9b65
Ngcali <magopeningcali@gmail.com>
2023-03-26 02:24:30 -0700

    4. A door is what a dog is perpetually on the wrong side of

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,22 +2,30 @@
 #define DOG_H
 
 /**
- * struct dog - a struct that represents a dog
- * @name: a pointer to the name of the dog
+ * struct dog - a dog structure
+ * @name: the name of the dog
  * @age: the age of the dog
- * @owner: a pointer to the name of the owner of the dog
+ * @owner: the owner of the dog
+ *
+ * Description: defines a dog structure with name, age, and owner
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
 
 /* Function prototypes */
-void init_dog(dog_t *d, char *name, float age, char *owner);
-void print_dog(dog_t *d);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
 #endif /* DOG_H */
+
