2ea762df6de8a175bdaf192328e5d3c70746b83e
Ngcali <magopeningcali@gmail.com>
2023-03-26 02:01:28 -0700

    0. Poppy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,20 +2,22 @@
 #define DOG_H
 
 /**
- * struct dog - a struct representing a dog
- * @name: the name of the dog
+ * struct dog - a struct that represents a dog
+ * @name: a pointer to the name of the dog
  * @age: the age of the dog
- * @owner: the name of the dog's owner
+ * @owner: a pointer to the name of the owner of the dog
  */
 typedef struct dog
 {
-    char name[100];
-    float age;
-    char owner[100];
+	char *name;
+	float age;
+	char *owner;
 } dog_t;
 
-/* initialize a dog struct variable with the given values */
+/* Function prototypes */
 void init_dog(dog_t *d, char *name, float age, char *owner);
+void print_dog(dog_t *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
 #endif /* DOG_H */
-
