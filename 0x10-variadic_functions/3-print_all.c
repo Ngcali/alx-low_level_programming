378f268c017529733f24d254299b17cc410ae247
Ngcali <magopeningcali@gmail.com>
2023-05-13 08:52:57 -0700

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,43 +11,45 @@
  * s: char * (if the string is NULL, print (nil) instead)
  * any other char should be ignored
  */
+
 void print_all(const char * const format, ...)
 {
-    va_list args;
-    int i = 0, j;
-    char *s;
+	va_list args;
+	int i = 0, j;
+	char *s;
 
-    va_start(args, format);
+	va_start(args, format);
 
-    while (format && format[i])
-    {
-        j = 1;
-        switch (format[i])
-        {
-            case 'c':
-                printf("%c", va_arg(args, int));
-                break;
-            case 'i':
-                printf("%d", va_arg(args, int));
-                break;
-            case 'f':
-                printf("%f", va_arg(args, double));
-                break;
-            case 's':
-                s = va_arg(args, char *);
-                if (!s)
-                    s = "(nil)";
-                printf("%s", s);
-                break;
-            default:
-                j = 0;
-                break;
-        }
-        if (format[i + 1] && j)
-            printf(", ");
-        i++;
-    }
-    printf("\n");
+	while (format && format[i])
+	{
+		j = 1;
+		
+		switch (format[i])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				s = va_arg(args, char *);
+				if (!s)
+					s = "(nil)";
+				printf("%s", s);
+				break;
+			default:
+				j = 0;
+				break;
+		}
+		if (format[i + 1] && j)
+			printf(", ");
+		i++;
+	}
+	printf("\n");
 
-    va_end(args);
+	va_end(args);
 }
