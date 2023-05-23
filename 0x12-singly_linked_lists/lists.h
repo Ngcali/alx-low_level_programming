d87aaa407885f06f542bddaed23e9f5399342863
Ngcali <magopeningcali@gmail.com>
2023-05-23 06:04:39 -0700

    Added lists.h file

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,15 +7,19 @@
  * @len: Length of the string
  * @next: Pointer to the next node
  */
+
 typedef struct list_s
 {
-    char *str;                 /* Pointer to the string data */
-    unsigned int len;          /* Length of the string */
-    struct list_s *next;       /* Pointer to the next node */
-} list_t;
+	char *str;
+	/*Pointer to the string data */
+	unsigned int len;
+	/* Length of the string */
+	struct list_s *next;
+	/* Pointer to the next node */
+}
+list_t;
 
 /* Function prototypes */
 size_t print_list(const list_t *h);
 
 #endif /* LIST_H */
-
