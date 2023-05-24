04307727374d595064418ba332f79b171d02290c
Ngcali <magopeningcali@gmail.com>
2023-05-24 02:02:27 -0700

    5. The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,5 @@ void print_message(void) __attribute__((constructor));
 void print_message(void)
 {
 	printf("You're beat! and yet, you must allow,
-	\nI bore my house upon my back!\n");
+			\nI bore my house upon my back!\n");
 }
