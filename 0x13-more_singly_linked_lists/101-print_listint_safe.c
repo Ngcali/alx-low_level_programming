5272b7b919e7206fe4f1ed4a2106f0fe0ce79c98
Ngcali <magopeningcali@gmail.com>
2023-05-27 03:31:54 -0700

    12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,26 +12,30 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
 	const listint_t *current, *loop_node;
+	const listint_t *visited[1000]; // Assuming a maximum of 1000 nodes in the list
+	int visitedCount = 0;
 
 	current = head;
 	while (current != NULL)
 	{
+		// Check if the current node has been visited before
+		for (int i = 0; i < visitedCount; i++) {
+			if (current == visited[i]) {
+				loop_node = current;
+				while (loop_node != current) {
+					count++;
+					printf("[%p] %d\n", (void *)loop_node, loop_node->n);
+					loop_node = loop_node->next;
+				}
+				printf("-> [%p] %d\n", (void *)loop_node, loop_node->n);
+				exit(98);
+			}
+		}
+
+		visited[visitedCount++] = current;
+
 		printf("[%p] %d\n", (void *)current, current->n);
 		count++;
-
-		/* Check for a loop */
-		if (current - current->next > 0)
-		{
-			loop_node = current->next;
-			while (current > loop_node)
-			{
-				count++;
-				printf("[%p] %d\n", (void *)loop_node, loop_node->n);
-				loop_node = loop_node->next;
-			}
-			printf("-> [%p] %d\n", (void *)loop_node, loop_node->n);
-			exit(98);
-		}
 
 		current = current->next;
 	}
