5dfa5644701e1b42f9a3f26f087a8f997b9c0cad
Ngcali <magopeningcali@gmail.com>
2023-05-27 04:03:18 -0700

    12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -3,31 +3,43 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - Prints a listint_t linked list
+ * print_listint_safe - Prints a listint_t linked list (safe version)
  * @head: Pointer to the head of the list
  *
- * Return: The number of nodes in the list
+ * Return: Number of nodes in the list
  */
 size_t print_listint_safe(const listint_t *head)
 {
-    size_t count = 0;
-    const listint_t *current = head, *temp = NULL;
+	const listint_t *slow, *fast;
+	size_t count = 0;
 
-    while (current)
-    {
-        printf("[%p] %d\n", (void *)current, current->n);
-        count++;
+	if (head == NULL)
+		return (0);
 
-        /* Check if the next node is already visited */
-        if (current >= current->next)
-        {
-            temp = current->next;
-            printf("-> [%p] %d\n", (void *)temp, temp->n);
-            break;
-        }
+	slow = head;
+	fast = head->next;
 
-        current = current->next;
-    }
+	while (fast != NULL && fast->next != NULL && fast > slow)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		count++;
 
-    return count;
+		slow = slow->next;
+		fast = fast->next->next;
+
+		if (slow == fast)
+		{
+			printf("-> [%p] %d\n", (void *)slow, slow->n);
+			count++;
+			break;
+		}
+	}
+
+	if (fast == NULL || fast->next == NULL)
+	{
+		printf("[%p] %d\n", (void *)slow, slow->n);
+		count++;
+	}
+
+	return (count);
 }
