c47b8a27c942d89f3dafbe486aad844138a9c4b6
Ngcali <magopeningcali@gmail.com>
2023-05-27 03:56:38 -0700

    12. Print (safe version)

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,28 +10,24 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	size_t count = 0;
-	const listint_t *current = head, *temp;
+    size_t count = 0;
+    const listint_t *current = head, *temp = NULL;
 
-	while (current)
-	{
-		printf("[%p] %d\n", (void *)current, current->n);
-		count++;
+    while (current)
+    {
+        printf("[%p] %d\n", (void *)current, current->n);
+        count++;
 
-		/* Check if the next node is already visited */
-		if (current >= current->next)
-		{
-			printf("-> [%p] %d\n", (void *)current->next, current->next->n);
-			break;
-		}
+        /* Check if the next node is already visited */
+        if (current >= current->next)
+        {
+            temp = current->next;
+            printf("-> [%p] %d\n", (void *)temp, temp->n);
+            break;
+        }
 
-		/* Mark the current node as visited */
-		temp = current;
-		current = current->next;
+        current = current->next;
+    }
 
-		/* Update the next pointer of the current node to point to itself */
-		temp->next = temp;
-	}
-
-	return count;
+    return count;
 }
